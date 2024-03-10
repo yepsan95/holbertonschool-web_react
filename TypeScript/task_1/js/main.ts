01e53c72b80ce6a55156e86f351b6f3d30ffb5f4
Bryan Yep <yepsan95@gmail.com>
2024-03-10 18:36:59 -0500

    feat: Task 4.

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -20,3 +20,30 @@ const printTeacher: PrintTeacherFunction = (firstName: string,
                                            ): string => {
   return `${firstName[0]}. ${lastName}`;
 };
+
+interface StudentConstructor {
+  new(firstName: string, lastName: string): StudentClass;
+}
+
+interface StudentInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
