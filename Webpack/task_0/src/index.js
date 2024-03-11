e33b2396e90ab408ec700b736c6da588cebe1824
Bryan Yep <yepsan95@gmail.com>
2024-03-10 21:41:27 -0500

    fix: JQuery structure is fixed.

--- Webpack/task_0/src/index.js
+++ Webpack/task_0/src/index.js
@@ -1,13 +1,5 @@
 import $ from 'jquery';
 
-const p1 = $('p');
-const p2 = $('p');
-const p3 = $('p');
-
-p1.text('Holberton Dashboard');
-p2.text('Dashboard data for the students');
-p3.text('Copyright - Holberton School');
-
-$(body).append(p1);
-$(body).append(p2);
-$(body).append(p3);
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
