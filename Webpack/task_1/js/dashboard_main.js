a841cd416f3fcdb7c177efde40fd2772a5273162
Bryan Yep <yepsan95@gmail.com>
2024-03-14 20:37:50 -0500

    fix: typo

--- Webpack/task_1/js/dashboard_main.js
+++ Webpack/task_1/js/dashboard_main.js
@@ -4,7 +4,7 @@ import _ from 'lodash';
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
-$('body').append(`<p> id='count'></p>`);
+$('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
 let clickCounter = 0;
