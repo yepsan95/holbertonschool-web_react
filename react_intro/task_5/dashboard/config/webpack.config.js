5c1f72c55dd91aa3d3abebdb686830c25140fa1e
Bryan Yep <yepsan95@gmail.com>
2024-03-21 16:10:15 -0500

    feat: Task 8.

---
+++ react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ]
+  },
+  devtool: 'inline-source-map',
+}
