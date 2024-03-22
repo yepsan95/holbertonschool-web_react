1aaffd6bc2937d6dd308e306442378f9b31918a8
Bryan Yep <yepsan95@gmail.com>
2024-03-21 19:05:39 -0500

    feat: Task 9.

--- react_intro/task_5/dashboard/config/webpack.config.js
+++ react_intro/task_5/dashboard/config/webpack.config.js
@@ -23,10 +23,22 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        }
+      },
     ],
   },
   devServer: {
-    contentBase: '../dist',
+    static: {
+      directory: path.join(__dirname, '../dist')
+    },
     compress: true,
     open: true,
     hot: true,
